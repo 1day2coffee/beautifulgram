cf5e6cd61652f941ec7a46516e9a0bf20c985fab
JYPARK <123rbdud@gmail.com>
2022-09-17 22:19:49 +0900

    ADD: Add login page

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Login from './component/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Login></Login>
 
       </header>
     </div>
